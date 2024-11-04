507b919175c62bd957667d43719843bc11d39416
Mr-A12k <kabil448446@gmail.com>
2024-11-04 21:29:19 +0530

    Message added

--- day2/SwapNum.java
+++ day2/SwapNum.java
@@ -7,12 +7,18 @@ public class SwapNum {
         int a = 10;
         int b = 5;
 
+        System.out.println("---Before swap---");
+        System.out.println("value of a is: " + a);
+        System.out.println("value of b is: " + b);
+
         a = a + b;
         b = a - b;
         a = a - b;
 
-        System.out.println("value of a is:" + a);
-        System.out.println("value of b is:" + b);
+        System.out.println("---After swap---");
+
+        System.out.println("value of a is: " + a);
+        System.out.println("value of b is: " + b);
     }
 
 }
