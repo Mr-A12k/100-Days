d1c9cd109eacb719ae65748a407e44aa248dad6f
Mr-A12k <kabil448446@gmail.com>
2024-11-06 22:31:05 +0530

    question added

--- day3/Parallelogram.java
+++ day3/Parallelogram.java
@@ -1,3 +1,18 @@
+// the output should be "java.lang.Exception: Breadth and height must be positive" without quotes.
+
+// Input Format
+
+// There are two lines of input. The first line contains : the breadth of the parallelogram. The next line contains : the height of the parallelogram.
+
+// Constraints
+
+// Output Format
+
+// If both values are greater than zero, then the main method must output the area of the parallelogram. Otherwise, print "java.lang.Exception: Breadth and height must be positive" without quotes.
+
+
+
+
 import java.io.*;
 import java.util.*;
 
