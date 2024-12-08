bdcf8a410ba8cb875765414c578925bd23c08e1e
Mr-A12k <kabil448446@gmail.com>
2024-12-08 13:19:47 +0530

    move zoro to the end of the array

---
+++ day5/MoveZeroToEnd.java
@@ -0,0 +1,27 @@
+package day5;
+
+import java.util.*;
+
+class MoveZeroToEnd{
+    public static void main(String args[]){
+        int[] a= {1,5,0,5,2,0,8};
+        for(int i=0; i<a.length; i++){
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+        int arr[] = moveZeroToEnd(a);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static int[] moveZeroToEnd(int a[]){
+        int count=0;
+        for(int i=0; i<a.length; i++){
+            if(a[i]!=0){
+                a[count++]=a[i];
+            }
+        }
+        while(count<a.length){
+            a[count++]=0;
+        }
+        return a;
+    }
+}
